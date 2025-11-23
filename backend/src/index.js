ae9ead785fc5728f720ee0fce06ef4bb97ccceff
Sneha pal <snehapal1708@gmail.com>
2025-11-23 20:09:12 +0530

    updated CORS for deployment

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,25 +4,21 @@ dotenv.config();
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-
 import path from "path";
 
 import { connectDB } from "./lib/db.js";
-
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { app, server } from "./lib/socket.js";
 
-
-
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 10000;
 const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "*", // temporary until frontend deployed
     credentials: true,
   })
 );
@@ -30,15 +26,7 @@ app.use(
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  });
-}
-
 server.listen(PORT, () => {
-  console.log("server is running on PORT:" + PORT);
+  console.log("server is running on PORT: " + PORT);
   connectDB();
 });
