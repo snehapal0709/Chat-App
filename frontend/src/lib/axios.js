c65d3611cfc57c3ef87f0e820e9e367273c11759
Sneha pal <snehapal1708@gmail.com>
2025-11-23 22:34:09 +0530

    error resolved

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
+console.log("API_URL = ", import.meta.env.VITE_API_URL);
+
+
 export const axiosInstance = axios.create({
-  baseURL: `${import.meta.env.VITE_API_URL}/api`,
+  baseURL: import.meta.env.VITE_API_URL + "/api",
   withCredentials: true,
 });
+
 
