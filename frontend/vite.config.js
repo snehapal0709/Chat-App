a8ee39b28a6c6319adc3ed5125032f7e815b7cd0
Sneha pal <snehapal1708@gmail.com>
2025-11-23 20:35:31 +0530

    error resolve

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      "/api": {
+        target: import.meta.env.VITE_API_URL,
+        changeOrigin: true,
+      },
+    },
+  },
   plugins: [react()],
 })
